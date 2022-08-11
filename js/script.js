0bc2d0ffb6c4bc74c31d1b2df3fc5f6b7970efe6
Gulshanoy <gulshanoydilshodovna@gmail.com>
2022-08-11 17:31:39 +0500

    Add wikipedia anded

--- js/script.js
+++ js/script.js
@@ -5,48 +5,48 @@ const button = document.getElementById('search-button')
 const searchResults = document.getElementById('results')
 
 //get value function
-const submitForm = (e)=>{
-    e.preventDefault()
-    let inputValue = searchInput.value
-    let query = inputValue.trim()
-    getResults(query)
+const submitForm = (e) => {
+  e.preventDefault()
+  let inputValue = searchInput.value
+  let query = inputValue.trim()
+  getResults(query)
 }
 
 //form event
 form.addEventListener('submit', submitForm)
 
 // get results function
-const getResults = (query)=>{
-    let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&origin=*&srlimit=25&utf8&format=json&srsearch=${query}`
-    try{
-        const req = await fetch(url)
-        const json = await req.json()
-        if(!json.query.search.length){
-            throw new Error(
-                `Kiritilgan so'rov bo'yicha hech qanday ma'lumot topilmadi, iltimos tekshirib qaytadan kiriting!!!`
-            )
-        }
-        addResults(json.query.search)
-    }catch(e){
-        searchResults.textContent = e.message
+const getResults = async (query) => {
+  let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&origin=*&srlimit=25&utf8&format=json&srsearch=${query}`
+  try {
+    const req = await fetch(url)
+    const json = await req.json()
+    if (!json.query.search.length) {
+      throw new Error(
+        'Kiritilgan so`rov bo`yicha hech qanday ma`lumot topilmadi',
+      )
     }
+    addResults(json.query.search)
+  } catch (e) {
+    searchResults.textContent = e.message
+  }
 }
 
 //
-const addResults = (results)=>{
-    searchResults.innerHTML = ''
+const addResults = (results) => {
+  searchResults.innerHTML = ''
 
-    results.forEach(result => {
-        const url = `https://en.wikipedia.org/?curid=${result.pageid}`
-        searchResults.insertAdjacentHTML(
-            "afterbegin",
-            `<div class='card'>
+  results.forEach((result) => {
+    const url = `https://en.wikipedia.org/?curid=${result.pageid}`
+    searchResults.insertAdjacentHTML(
+      'afterbegin',
+      `<div class='card'>
                 <h3 class = 'card-title'>
                     <a href="${url}" target="_blank">${result.title}</a>
                 </h3>
                 <a class='card-link' href="${url}" target='_blank'>${url}</a>
                 <span class='card-snippet'>${result.snippet}</span>
-            </div>`
-        )
-    });
+            </div>`,
+    )
+  })
 }
